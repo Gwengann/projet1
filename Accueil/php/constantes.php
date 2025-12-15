6ad4017b1ceaa656aa148155391b0ca6d64fa583
Gwengann <lecorvic.g@gmx.fr>
2025-12-15 11:20:57 +0100

    Update constantes.php

--- Accueil/php/constantes.php
+++ Accueil/php/constantes.php
@@ -1,5 +1,5 @@
 <?php 
-define('DB_USER', 'comweb_project');
-define('DB_PASSWORD', 'pt_bewmoc_isen29');
-define('DB_NAME', 'comweb_project');
-define('DB_SERVER', 'localhost');
+define('DB_USER', 'toset');
+define('DB_PASSWORD', 'toset');
+define('DB_NAME', 'toset');
+define('DB_SERVER', 'toset');
