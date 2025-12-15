ee1ac1a2bec7146ced9f28219f7de3bd87b36aa8
Gwengann <lecorvic.g@gmx.fr>
2025-12-15 11:12:03 +0100

    Update constants.php

--- Authentification/php/constants.php
+++ Authentification/php/constants.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * @Author: Thibault Napoléon <Imothep>
- * @Company: ISEN Yncréa Ouest
- * @Email: thibault.napoleon@isen-ouest.yncrea.fr
- * @Created Date: 22-Jan-2018 - 14:18:14
- * @Last Modified: 13-Dec-2019 - 21:31:52
- */
-
   // Database constants.
   define('DB_USER', 'comweb_tp');
   define('DB_PASSWORD', 'pt_bewmoc_isen29');
